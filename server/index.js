c382ff820e8d26056f4333aa4a44263366dd84ef
Satakshivdhy <satakshivaidhy90@gmail.com>
2026-07-20 19:52:09 +0530

    refactor: Update API endpoint for profile editing to use common route

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 app.use("/common", CommonRouter);
-app.use("/user", CommonRouter);
+// app.use("/user", CommonRouter);
 app.use("/restaurant", RestaurantRouter);
 //Default API
 app.get("/", (req, res) => {
